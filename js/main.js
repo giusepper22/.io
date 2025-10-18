57885c6a5c9bced8c048afe4e6462a654f7ab5e0
gr <74497534+JoeSmit22@users.noreply.github.com>
2025-10-18 13:22:36 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,42 @@ $(document).ready(() => {
   $(() => {
     $('[data-toggle="tooltip"]').tooltip();
   });
+}); 
+
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 40
+    },
+    "color": {
+      "value": "#000000"
+    },
+    "opacity": {
+      "value": 0.4
+    },
+    "size": {
+      "value": 3
+    },
+    "line_linked": {
+      "enable": true,
+      "color": "#000000",
+      "opacity": 0.1
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.6
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true
+      },
+      "onclick": {
+        "enable": true
+      }
+    }
+  },
+  "retina_detect": true
 });
